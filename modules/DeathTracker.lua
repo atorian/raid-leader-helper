6237805d257d8b322f0dcd8ea7582c76f173c876
atorian <atorian.ak@gmail.com>
2025-05-27 22:20:55 +0200

    fix death tracker

--- modules/DeathTracker.lua
+++ modules/DeathTracker.lua
@@ -47,8 +47,9 @@ function DeathTracker:logHeal(playerName, event)
     self.healEvents[playerName] = event
 end
 
+-- COMBATLOG_OBJECT_TYPE_PLAYER
 function DeathTracker:handleEvent(eventData, log)
-    if bit.band(eventData.destFlags, COMBATLOG_OBJECT_TYPE_PLAYER) > 0 then
+    if bit.band(eventData.destFlags, COMBATLOG_OBJECT_AFFILIATION_RAID) > 0 then
         if eventData.event == "SPELL_DAMAGE" then
             self:logDmg(eventData.destName, {
                 source = eventData.sourceName,
@@ -65,7 +66,6 @@ function DeathTracker:handleEvent(eventData, log)
         elseif eventData.event == "UNIT_DIED" then
             self:ProcessPlayerDeath(log, eventData.destName, eventData.timestamp)
         end
-        -- self:ProcessPlayerDeath(log, eventData.destName, eventData.timestamp)
     end
 end
 
@@ -89,44 +89,4 @@ function DeathTracker:ProcessPlayerDeath(log, playerName, timestamp)
             log(playerName, msg)
         end
     end
-end
-
--- -- Функция для парсинга временной метки из лога в секунды
--- function DeathTracker:ParseTimestamp(timestamp)
---     -- Формат времени в логе: MM/DD HH:mm:ss.SSS
---     local _, _, month, day, hour, min, sec, ms = string.find(timestamp, "(%d+)/(%d+)%s+(%d+):(%d+):(%d+)%.(%d+)")
---     if month and day and hour and min and sec and ms then
---         return hour * 3600 + min * 60 + sec + ms / 1000
---     end
---     return 0
--- end
-
--- function DeathTracker:GetHealthPercent(unitName)
---     local unit = self:GetUnitByName(unitName)
---     if unit then
---         local health = UnitHealth(unit)
---         local maxHealth = UnitHealthMax(unit)
---         if health and maxHealth and maxHealth > 0 then
---             return math.floor((health / maxHealth) * 100)
---         end
---     end
---     return 0
--- end
-
--- function DeathTracker:GetTimeSinceHeal(playerName)
---     local lastHeal = self.lastHealEvents:getAll()[1]
---     if lastHeal and lastHeal.target == playerName then
---         return math.floor(GetTime() - self:ParseTimestamp(lastHeal.timestamp))
---     end
---     return 0
--- end
-
-function DeathTracker:GetUnitByName(name)
-    local units = {"player", "party1", "party2", "party3", "party4", "raid1", "raid2", "raid3", "raid4", "raid5"}
-    for _, unit in ipairs(units) do
-        if UnitName(unit) == name then
-            return unit
-        end
-    end
-    return nil
 end
